86d399b3ba384d8c8c3c8c6e9686009f165b565d
brown <86018533+2brownc@users.noreply.github.com>
2024-05-22 03:21:15 +0530

    API - udpate API endpoints to reflect changes in the backend server: closes #17

--- src/api/favs.ts
+++ src/api/favs.ts
@@ -1,113 +1,113 @@
-import axios from "axios"
-import type { ImageId, ImageRecord } from "../types"
+import axios from "axios";
+import type { ImageId, ImageRecord } from "../types";
 
 export async function setFavorite(
-	userId: string,
-	imageRecord: ImageRecord,
-	accessToken: string,
+  userId: string,
+  imageRecord: ImageRecord,
+  accessToken: string
 ): Promise<boolean> {
-	// URI for setFavoriteImage endpoint on server
-	const uri = `${import.meta.env.VITE_SERVER_URL}/setFavoriteImage`
+  // URI for setFavoriteImage endpoint on server
+  const uri = `${import.meta.env.VITE_SERVER_URL}/images/setFavorite`;
 
-	// Request headers with auth token
-	const headers = {
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${accessToken}`,
-		"Access-Control-Allow-Origin": "*",
-	}
+  // Request headers with auth token
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+    "Access-Control-Allow-Origin": "*",
+  };
 
-	// Request body with user ID and image record
-	const body = {
-		userId,
-		imageRecord,
-	}
+  // Request body with user ID and image record
+  const body = {
+    userId,
+    imageRecord,
+  };
 
-	try {
-		// Make POST request to set favorite image
-		const response = await axios.post(uri, body, { headers })
+  try {
+    // Make POST request to set favorite image
+    const response = await axios.post(uri, body, { headers });
 
-		// Handle 200 response
-		if (response.status === 200) {
-			return true
-		}
-	} catch (error) {
-		// Log any errors
-		console.error(error)
-	}
+    // Handle 200 response
+    if (response.status === 200) {
+      return true;
+    }
+  } catch (error) {
+    // Log any errors
+    console.error(error);
+  }
 
-	// Default return value if request fails
-	return false
+  // Default return value if request fails
+  return false;
 }
 
 export async function unsetFavorite(
-	userId: string,
-	imageId: ImageId,
-	accessToken: string,
+  userId: string,
+  imageId: ImageId,
+  accessToken: string
 ): Promise<boolean> {
-	// URI for unsetFavoriteImage endpoint on server
-	const uri = `${import.meta.env.VITE_SERVER_URL}/unsetFavoriteImage`
+  // URI for unsetFavoriteImage endpoint on server
+  const uri = `${import.meta.env.VITE_SERVER_URL}/images/unsetFavorite`;
 
-	// Request headers with auth token
-	const headers = {
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${accessToken}`,
-		"Access-Control-Allow-Origin": "*",
-	}
+  // Request headers with auth token
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+    "Access-Control-Allow-Origin": "*",
+  };
 
-	// Request body with user ID and image record
-	const body = {
-		userId,
-		imageId,
-	}
+  // Request body with user ID and image record
+  const body = {
+    userId,
+    imageId,
+  };
 
-	try {
-		// Make POST request to set favorite image
-		const response = await axios.post(uri, body, { headers })
+  try {
+    // Make POST request to set favorite image
+    const response = await axios.post(uri, body, { headers });
 
-		// Handle 200 response
-		if (response.status === 200) {
-			return true
-		}
-	} catch (error) {
-		// Log any errors
-		console.error("Error unsetting favorite image:", error)
-	}
+    // Handle 200 response
+    if (response.status === 200) {
+      return true;
+    }
+  } catch (error) {
+    // Log any errors
+    console.error("Error unsetting favorite image:", error);
+  }
 
-	// Default return value if request fails
-	return false
+  // Default return value if request fails
+  return false;
 }
 
 export async function getFavorites(
-	userId: string,
-	accessToken: string,
+  userId: string,
+  accessToken: string
 ): Promise<ImageRecord[] | null> {
-	// URI for getFavorites endpoint on server
-	const uri = `${import.meta.env.VITE_SERVER_URL}/getFavoriteImages`
+  // URI for getFavorites endpoint on server
+  const uri = `${import.meta.env.VITE_SERVER_URL}/images/getFavorite`;
 
-	// Request headers with auth token
-	const headers = {
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${accessToken}`,
-		"Access-Control-Allow-Origin": "*",
-	}
+  // Request headers with auth token
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+    "Access-Control-Allow-Origin": "*",
+  };
 
-	const body = {
-		userId,
-	}
+  const body = {
+    userId,
+  };
 
-	try {
-		// Make POST request to get favorite images
-		const response = await axios.post(uri, body, { headers })
+  try {
+    // Make POST request to get favorite images
+    const response = await axios.post(uri, body, { headers });
 
-		// Handle 200 response
-		if (response.status === 200) {
-			return response.data as ImageRecord[]
-		}
-	} catch (error) {
-		// Log any errors
-		console.error("Error unsetting favorite image:", error)
-	}
+    // Handle 200 response
+    if (response.status === 200) {
+      return response.data as ImageRecord[];
+    }
+  } catch (error) {
+    // Log any errors
+    console.error("Error unsetting favorite image:", error);
+  }
 
-	// Default return value if request fails
-	return null
+  // Default return value if request fails
+  return null;
 }
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,84 +1,84 @@
-import axios from "axios"
-import type { User } from "@auth0/auth0-react"
+import axios from "axios";
+import type { User } from "@auth0/auth0-react";
 
 export async function updateUserMetadata(
-	user: User,
-	metadata: object,
-	accessToken: string,
+  user: User,
+  metadata: object,
+  accessToken: string
 ) {
-	const { sub, app_metadata, user_metadata } = user
+  const { sub, app_metadata, user_metadata } = user;
 
-	const updatedMetadata = {
-		...app_metadata,
-		...user_metadata,
-		...metadata,
-	}
+  const updatedMetadata = {
+    ...app_metadata,
+    ...user_metadata,
+    ...metadata,
+  };
 
-	const headers = {
-		Authorization: `Bearer ${accessToken}`,
-		"Content-Type": "application/json",
-	}
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  };
 
-	const body = {
-		user_metadata: updatedMetadata,
-	}
+  const body = {
+    user_metadata: updatedMetadata,
+  };
 
-	const uri = `https://${import.meta.env.VITE_AUTH0_DOMAIN}/api/v2/users/${sub}`
+  const uri = `https://${import.meta.env.VITE_AUTH0_DOMAIN}/api/v2/users/${sub}`;
 
-	const response = await axios.patch(uri, { headers, body })
+  const response = await axios.patch(uri, { headers, body });
 
-	return response.data
+  return response.data;
 }
 
 export async function createUser(
-	userId: string,
-	userName: string,
-	accessToken: string,
+  userId: string,
+  userName: string,
+  accessToken: string
 ) {
-	const body = {
-		userId: userId,
-		userName: userName,
-	}
+  const body = {
+    userId: userId,
+    userName: userName,
+  };
 
-	const headers = {
-		Authorization: `Bearer ${accessToken}`,
-		"Content-Type": "application/json",
-		"Access-Control-Allow-Origin": `${
-			import.meta.env.VITE_SERVER_CROSS_ORIGIN
-		}`,
-	}
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": `${
+      import.meta.env.VITE_SERVER_CROSS_ORIGIN
+    }`,
+  };
 
-	const uri = `${import.meta.env.VITE_SERVER_URL}/createUser`
+  const uri = `${import.meta.env.VITE_SERVER_URL}/user/create`;
 
-	try {
-		await axios.post(uri, body, { headers })
-	} catch (error) {
-		return false
-	}
+  try {
+    await axios.post(uri, body, { headers });
+  } catch (error) {
+    return false;
+  }
 
-	return true
+  return true;
 }
 
 export async function getUserProfile(userId: string, accessToken: string) {
-	const body = {
-		userId,
-	}
+  const body = {
+    userId,
+  };
 
-	const headers = {
-		Authorization: `Bearer ${accessToken}`,
-		"Content-Type": "application/json",
-		"Access-Control-Allow-Origin": `${
-			import.meta.env.VITE_SERVER_CROSS_ORIGIN
-		}`,
-	}
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": `${
+      import.meta.env.VITE_SERVER_CROSS_ORIGIN
+    }`,
+  };
 
-	const uri = `${import.meta.env.VITE_SERVER_URL}/userProfile`
+  const uri = `${import.meta.env.VITE_SERVER_URL}/user/profile`;
 
-	try {
-		const response = await axios.post(uri, body, { headers })
-		return response.data
-	} catch (error) {
-		// if user is not found, return null
-		return null
-	}
+  try {
+    const response = await axios.post(uri, body, { headers });
+    return response.data;
+  } catch (error) {
+    // if user is not found, return null
+    return null;
+  }
 }
